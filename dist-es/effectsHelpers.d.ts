2df8c204bbf6a33b88dd5090af6c810ccfd3ee68
Bas Horsting <bas.horsting@greetz.com>
2022-03-11 14:05:01 +0100

    Version 15.4.6 Add getMaskedLayerSize.

--- dist-es/effectsHelpers.d.ts
+++
@@ -1,5 +0,0 @@
-import { LayerEffectsInfo } from './psd';
-import { PsdReader } from './psdReader';
-import { PsdWriter } from './psdWriter';
-export declare function readEffects(reader: PsdReader): LayerEffectsInfo;
-export declare function writeEffects(writer: PsdWriter, effects: LayerEffectsInfo): void;
