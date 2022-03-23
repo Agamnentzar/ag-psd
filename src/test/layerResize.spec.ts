f9a57fe56c662069944ee0dda7fe963c0dbf4211
Bas Horsting <bas@greetz.com>
2022-03-23 16:48:59 +0100

    Remove debug stuff

--- src/test/layerResize.spec.ts
+++ src/test/layerResize.spec.ts
@@ -3,7 +3,6 @@ import {createCanvas, createCanvasFromData, initializeCanvas} from '../helpers';
 import {Psd} from '../psd';
 import {getMaskedLayerSize} from '../index';
 import {readPsdFromFile} from './common';
-import * as fs from 'fs';
 
 const psdFileToTest = './test-manual/masked_layer_size_2.psd';
 initializeCanvas(createCanvas, createCanvasFromData);
@@ -15,17 +14,7 @@ describe('When getting the size of a layer mask that extends to the left of the 
 	});
 	it('Should crop layer size correctly', async () => {
 		const layer = psd.children![4];
-		const imageContent = await (<any>layer).canvas.toBuffer();
-		fs.writeFileSync(
-			`test1.png`,
-			imageContent,
-		);
-		const maskContent = await (<any>layer).mask.canvas.toBuffer();
-		fs.writeFileSync(
-			`test1_mask.png`,
-			maskContent,
-		);
-		const layerSize = getMaskedLayerSize(psd.children![4], 10, psd);
+		const layerSize = getMaskedLayerSize(layer, 10, psd);
 		expect(psd.width).to.equal(1000);
 		expect(psd.height).to.equal(1000);
 		expect(layerSize.left).to.equal(50);
