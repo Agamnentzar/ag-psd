aecbc48735fc894fbbf7bdcc272feff7febc281c
Bas Horsting <bas.horsting@greetz.com>
2022-03-31 16:21:03 +0200

    Bump version

--- src/initializeCanvas.ts
+++ src/initializeCanvas.ts
@@ -1,13 +1,16 @@
-import { createCanvas } from './canvas/Canvas';
-import {Image} from 'skia-canvas/lib';
+import {Canvas, Image} from 'skia-canvas/lib';
 import { initializeCanvas } from './index';
 
 function createCanvasFromData(data: Uint8Array) {
 	const image = new Image();
 	image.src = <any>Buffer.from(data);
-	const canvas = createCanvas(image.width, image.height);
+	const canvas = new Canvas(image.width, image.height);
 	canvas.getContext('2d')!.drawImage(image, 0, 0);
 	return canvas;
+}
+
+function createCanvas(width: number, height: number): Canvas {
+	return new Canvas(width, height);
 }
 
 initializeCanvas(createCanvas, createCanvasFromData);
